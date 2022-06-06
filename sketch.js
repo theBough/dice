03ea3c78600d21d7fbd4b7b3953a2d25829307be
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-06 14:41:45 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let rndNum = 0;
+let rollBtn;
+let rollCounter = 0;
+let isRolling = false;
+function setup() {
+  createCanvas(400, 400);
+  rndNum = Math.ceil(random(6));
+  rollBtn = createButton("Roll");
+  rollBtn.mousePressed(newRoll);
+}
+function draw() {
+  background(220);
+  textSize(50);
+  text(rndNum, 100, 100);
+  if (isRolling) {
+    rndNum = Math.ceil(random(6));
+    rollCounter += 1;
+    if (rollCounter > 30) {
+      //stop the rolling
+      rndNum = Math.ceil(random(6));
+      isRolling = false;
+      rollCounter = 0;
+    }
+  }
+}
+function newRoll() {
+  isRolling = true; 
+}
