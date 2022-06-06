daf48094ce60b8533a88db1bf2114c40e5a78f70
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-06 14:54:28 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,15 @@ let rndNum = 0;
 let rollBtn;
 let rollCounter = 0;
 let isRolling = false;
+let inp
 function setup() {
   createCanvas(400, 400);
   rndNum = Math.ceil(random(6));
   rollBtn = createButton("Roll");
   rollBtn.position(10,10)
   rollBtn.mousePressed(newRoll);
+  inp = createInput("Place your bet");
+  inp.position(10,30)
 }
 function draw() {
   background(220);
